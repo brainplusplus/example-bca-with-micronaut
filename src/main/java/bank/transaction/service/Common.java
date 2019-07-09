9a8d0c317bfc4cdfac219fa17c1d3d3601ca03ef
Bobby <15326935+akyong@users.noreply.github.com>
2019-07-09 10:43:01 +0700

    Update Common.java

--- src/main/java/bank/transaction/service/Common.java
+++ src/main/java/bank/transaction/service/Common.java
@@ -18,19 +18,4 @@ public class Common {
 
 
 
-//    public final String BCA_URL = "https://sandbox.bca.co.id";
-//    public final String BCA_API_KEY = "a4a5403b-a49e-48e6-9213-c72ccfcf54eb";
-//    public final String BCA_API_SECRET = "70ead5e3-15e1-431f-9774-a4d4efe9ba16";
-//    public final String BCA_CLIENT_ID = "e571d7fd-9b5b-46e8-a835-2f5793c44611";
-//    public final String BCA_CLIENT_SECRET = "cb711807-bd70-4069-a6ce-bb9739716cae";
-//    public final String BCA_CORPORATE_ID = "BCAAPI2016";
-//    public final String BCA_ACCOUNT_NUMBER = "0201245680";
-
-
-    /**
-     * Notification Server URL
-     * */
-
-
-
 }
